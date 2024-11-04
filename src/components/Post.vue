7082691630fd04f2db0a2378b7536d284faf126f
micky-code <solaisan22@.com>
2024-11-04 11:40:04 +0700

    update

---
+++ src/components/Post.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <div v-html="content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    content: String,
+  },
+};
+</script>
--- src/content/blog/what-about-me.md
+++
@@ -1,7 +0,0 @@
----
-title: What about me
-date: 2024-11-04T10:48:00.000Z
-author: fhsgerg
-content: wagwwffw
-image: /uploads/screenshot-2024-10-16-191445.png
----
